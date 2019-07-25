9b07344d9bbd10de0ab40974c461307d8c3e4172
Sergey Lazarenkov <real3060@gmail.com>
2019-07-25 20:50:32 +0300

    commit 1

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,15 @@
 import { Component } from '@angular/core';
+import {Model} from "./model";
 
 @Component({
-  selector: 'app-root',
+  selector: 'todo-app',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.less']
 })
 export class AppComponent {
   title = 'ang-book-todo';
+  model = new Model();
+  getName() {
+    return this.model.user
+  }
 }
